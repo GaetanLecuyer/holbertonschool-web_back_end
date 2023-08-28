63d4edf6dc13b2dc9b312a4ce14b75193295266f
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-08-28 18:42:57 +0200

    add floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 2:
+Returns the largest integer less than or equal to the input number.
+"""
+import math
+
+def floor(n: float) -> float:
+    """
+    Returns the floor of a given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        float: The floor of the input float.
+    """
+    return math.floor(n)
