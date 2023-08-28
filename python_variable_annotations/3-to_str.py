429464b82c8ac5e755700627fff4d49527d79681
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-08-28 18:49:08 +0200

    converti to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 3:
+Convert a float number to a string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
