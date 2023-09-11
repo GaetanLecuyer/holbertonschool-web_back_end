99346e7b60c56ccce93ac084d8486df43ce77efd
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-09-11 10:33:38 +0200

    add the comment

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,21 @@
+// Cette fonction déclare une variable 'task' avec 'const' et lui attribue une valeur.
+// Elle renvoie ensuite cette valeur.
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+// Cette fonction renvoie une chaîne de caractères.
+export function getLast() {
+  return ' is okay';
+}
+
+// Cette fonction déclare une variable 'combination' avec 'let', lui attribue une valeur,
+// puis ajoute la valeur renvoyée par la fonction 'getLast' à 'combination'.
+// Enfin, elle renvoie 'combination'.
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
