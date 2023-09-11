58a9e87f775aa285f8775ea9f9a1bd05baffd3e0
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-09-11 10:40:43 +0200

    add

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export function taskBlock() {
+  // Declare variables with let or const
+  let task = 'I prefer const when I can.';
+
+  if (true) {
+    const task = 'But sometimes let';
+    console.log(task); // This will log 'But sometimes let'
+  }
+
+  // task here refers to the outer variable, not the one inside the conditional block
+  return task; // This will return 'I prefer const when I can.'
+}
