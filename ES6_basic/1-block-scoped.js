870c2f5d7672aef2cffe27436e07b5ef0ac61bc5
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-09-11 14:21:36 +0200

    add task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Use let instead of var
-  let task2 = true;  // Use let instead of var
+  const task = false;  // Use let instead of var
+  const task2 = true;  // Use let instead of var
 
   if (trueOrFalse) {
-    task = true;      // No need to redeclare with let, just assign the new value
-    task2 = false;    // No need to redeclare with let, just assign the new value
+    const task = true;      // No need to redeclare with let, just assign the new value
+    const task2 = false;
+    return [task2, task];     // No need to redeclare with let, just assign the new value
   }
 
   return [task, task2];
